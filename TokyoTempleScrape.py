7bb52a3d809d69954d62ea47ccab014bfc238084
Guilherme Martins <martinsg88@gmail.com>
2017-07-02 21:14:29 +0900

    modified:   TokyoTempleScrape.py

--- TokyoTempleScrape.py
+++ TokyoTempleScrape.py
@@ -3,23 +3,32 @@ import requests
 from StringIO import StringIO
 
 
-def __initialize_data():
-    url = "https://hotokami.jp/area/tokyo/"
+def get_url_main_site():
+    return "https://hotokami.jp"
+
+
+def __load_url(url):
     r = requests.get(url)
     data = r.text
     print data
     return etree.parse(StringIO(data), etree.HTMLParser()) 
 
 
+def __initialize_data():
+    url = "https://hotokami.jp/area/tokyo/"
+    return __load_url(url) 
+
+
 def __get_list_of_pages_tokyo():
     tree = __initialize_data() 
     listOfTemples = tree.xpath('//*[@id="content"]/div/section[*]/h3/a')
     for ele in listOfTemples:
-        print ele.attrib['href']
+        __click_on_temple_pages(ele.attrib['href'])
 
 
-def __click_on_temple_pages():
-    pass
+def __click_on_temple_pages(partial):
+    __load_url(get_url_main_site() + partial)
+    
 
 
 if __name__ == "__main__":
